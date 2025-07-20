4590d19cdc5a6b22890a7956a17c787b3ac8a828
NotAlexNoyle <alexnoyle@icloud.com>
2025-07-20 11:53:18 -0400

    Rename project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,59 @@
 plugins {
-    id("java")
-    id("java-library")
-    id("com.diffplug.spotless") version "7.0.4"
-    id("com.gradleup.shadow") version "8.3.6"
-    eclipse
+    id("java") // Tell gradle this is a java project.
+    id("java-library") // Import helper for source-based libraries.
+    kotlin("jvm") version
+        "2.1.21" // Import kotlin jvm plugin for kotlin/java integration (Required for DiamondBank-OG API)
+    id("com.diffplug.spotless") version "7.0.4" // Import auto-formatter.
+    id("com.gradleup.shadow") version "8.3.6" // Import shadow API.
+    eclipse // Import eclipse plugin for IDE integration.
 }
 
-java { sourceCompatibility = JavaVersion.VERSION_17 }
+java {
+    sourceCompatibility = JavaVersion.VERSION_17 // Compile with JDK 17 compatibility.
 
-group = "net.lapismc"
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17)) // Use JDK 17.
+        vendor.set(JvmVendorSpec.GRAAL_VM) // Use GraalVM CE.
+    }
+}
 
-version = "1.0.6"
+kotlin { jvmToolchain(17) }
 
-val apiVersion = "1.19"
+group = "net.true-og.DiamondBank-OG-PAPI" // Declare bundle identifier.
+
+version = "1.0.7" // Declare plugin version (will be in .jar).
+
+val apiVersion = "1.19" // Declare minecraft server target version.
 
 tasks.named<ProcessResources>("processResources") {
     val props = mapOf("version" to version, "apiVersion" to apiVersion)
-
-    inputs.properties(props)
-
+    inputs.properties(props) // Indicates to rerun if version changes.
     filesMatching("plugin.yml") { expand(props) }
-    from("LICENSE") { into("/") }
+    from("LICENSE") { into("/") } // Bundle license into .jars.
 }
 
 repositories {
     mavenCentral()
     gradlePluginPortal()
-    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
+    maven { url = uri("https://repo.purpurmc.org/snapshots") }
     maven { url = uri("https://maven.lapismc.net/repository/maven/") }
     maven { url = uri("https://oss.sonatype.org/content/groups/public/") }
     maven { url = uri("https://repo.helpch.at/releases/") }
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
-    compileOnly("net.lapismc:AFKPlus:3.4.3")
+    compileOnly("org.purpurmc.purpur:purpur-api:1.19.4-R0.1-SNAPSHOT") // Declare purpur API version to be packaged.
     compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnlyApi(project(":libs:DiamondBank-OG")) // Import TrueOG Network DiamondBank-OG API.
 }
 
-tasks.withType<AbstractArchiveTask>().configureEach {
+tasks.withType<AbstractArchiveTask>().configureEach { // Ensure reproducible .jars
     isPreserveFileTimestamps = false
     isReproducibleFileOrder = true
 }
 
 tasks.shadowJar {
-    archiveClassifier.set("")
+    archiveClassifier.set("") // Use empty string instead of null.
     minimize()
 }
 
@@ -56,17 +65,9 @@ tasks.build {
 tasks.jar { archiveClassifier.set("part") }
 
 tasks.withType<JavaCompile>().configureEach {
-    options.compilerArgs.add("-parameters")
-    options.compilerArgs.add("-Xlint:deprecation")
+    options.compilerArgs.addAll(listOf("-parameters", "-Xlint:deprecation")) // Triggers deprecation warning messages.
     options.encoding = "UTF-8"
     options.isFork = true
-}
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
-        vendor = JvmVendorSpec.GRAAL_VM
-    }
 }
 
 spotless {
